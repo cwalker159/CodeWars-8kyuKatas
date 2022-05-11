a4a86602e0e04b982bb262e191ce45a810caf146
Carl R. Walker Jr <96443329+cwalker159@users.noreply.github.com>
2022-05-11 11:27:16 -0400

    CodeWars Python Solutions
    The volume of a Cuboid
    Given Code
    def getVolumeOfCubiod(length, width, height):
        Code goes here...

---
+++ solution002.py
@@ -0,0 +1,10 @@
+# CodeWars Python Solutions
+# Volume of a Cuboid
+# Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation.
+# Given Code
+# def getVolumeOfCubiod(length, width, height):
+    # Code goes here...
+
+# Solution
+def getVolumeOfCubiod(length, width, height):
+    return length * width * height
